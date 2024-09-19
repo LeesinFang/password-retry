6ad2f00ae03665ca0a38853f3940ae38b57948e0
LeesinFang <116412975+LeesinFang@users.noreply.github.com>
2024-09-19 22:53:06 +0800

    Update password.py

--- password.py
+++ password.py
@@ -1,13 +1,13 @@
 #密碼重設程式
 password = 'a123456'
 i = 3
-while True:
+while i > 0:
+	i = i-1
 	pwd = input('請輸入密碼: ') #訂正
 	if pwd == password:
 		print('登入成功!')
 		break 	
 	else:
-		i = i-1
-		print('密碼錯誤! 還有' , i, '次機會') #切三段 中間放整數
-		if i == 0:
-			break
+		print('密碼錯誤!') 
+		if i > 0:
+			print('還有', i, '次機會')
